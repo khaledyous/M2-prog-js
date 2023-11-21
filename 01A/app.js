b78161e3df32a411b58e27e539fa589250bb6c4a
khaled <khaledyoussef2019@outlook.com>
2023-11-21 12:09:21 +0100

    klaar

--- 01A/app.js
+++ 01A/app.js
@@ -3,6 +3,21 @@ class App
     runApplication()
     {
         console.log("Hello world");
+        let appNaam = "MijnApplicatie";
+        let versienummer = 0.9;
+        let versiedatum = "2023-11-23";
+        let auteur =  "Khaled" 
+        let copyright = "bedrijf" 
+        let distributeur = "Jouw Distributeur";
+        let darkmode = false;    
+
+        console.log("Applicatie naam:", appNaam);
+        console.log("Versienummer:", versienummer);
+        console.log("Versiedatum:", versiedatum);
+        console.log("Auteur:", auteur);
+        console.log("Copyright:", copyright);
+        console.log("Distributeur:", distributeur);
+        console.log("Darkmode:", darkmode);
     }
     
 }
