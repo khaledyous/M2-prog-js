c217be93459abcec4884fa05a6dfabb9d9e9ef6c
khaled <khaledyoussef2019@outlook.com>
2023-11-23 12:28:24 +0100

    done

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,6 +6,9 @@ class App
 
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
+        g.fillStyle = "yellow";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "black";
         g.fillRect(0,0,10,10);
 
         console.log(canvas)
